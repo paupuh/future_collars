4690de8b444a2d459cdd205ff672d35c790fa903
paupuh <116916759+paupuh@users.noreply.github.com>
2025-01-29 22:18:18 +0100

    exercisse done

---
+++ potegowanie.js
@@ -0,0 +1,17 @@
+function power(base, exponent) {
+  if (typeof base === "number" && typeof exponent === "number")
+    console.log("Arguments are correct");
+  return Math.pow(base, exponent);
+}
+console.log(power(2, 3));
+
+// Stwórz funkcję pow, która będzie wykonywała działanie potęgowania. Funkcja będzie przyjmowała dwa argumenty typu number.
+// Pierwszym parametrem będzie podstawa potęgi, a drugim wykładnik. Funkcja powinna zwrócić wykonaną operację.
+
+// Przykładowy input:
+
+// Cyfry 2 i 2, cyfry 3 i 3
+
+// Przykładowy output:
+
+// 4, 27
